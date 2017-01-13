67824efd3675a3550a8ee7dcf8253885ddf7666c
Glen Costigan <gcostiga@vols.utk.edu>
2017-01-13 14:02:03 -0500

    made class structure format

---
+++ main.py
@@ -0,0 +1,10 @@
+from parts import *
+
+def abort():
+
+
+if __name__ = "__main__":
+    t = trencher()
+    i = ice_extracter()
+    r = railing()
+
---
+++ parts.py
@@ -0,0 +1,28 @@
+import wiringpi
+
+class trencher:
+    def tilt(self, servo, amount):
+
+
+    def spin(self, servo, amount):
+
+
+    def origin(self):
+
+
+
+class ice_extracter:
+    def heat(self, amount):
+
+
+    def move_z(self, amount):
+
+
+
+class railing:
+    def move_x(self, amount):
+
+
+    def move_y(self, amount):
+
+
