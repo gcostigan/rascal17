844ca73f703b32cf3c79837658cc8aa3421cc35d
glen-costigan <gcostiga@vols.utk.edu>
2017-01-13 16:59:30 -0500

    test

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from parts import *
+from xbox import controller
 
 def abort():
     return
+
+def manual_control()
 
 if __name__ == "__main__":
     t = trencher()
     i = ice_extracter()
     r = railing()
+    c = controller()
+    manual_control()
 
