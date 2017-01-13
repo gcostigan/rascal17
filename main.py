615ceb23f8e6ac6a8b26316c4eca2291722bf629
glen-costigan <gcostiga@vols.utk.edu>
2017-01-13 14:07:48 -0500

    less errors now

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from parts import *
 
 def abort():
+    return
 
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     t = trencher()
     i = ice_extracter()
     r = railing()
--- parts.py
+++ parts.py
@@ -2,26 +2,25 @@ import wiringpi
 
 class trencher:
     def tilt(self, servo, amount):
-
+        return
 
     def spin(self, servo, amount):
-
+        return
 
     def origin(self):
-
+        return
 
 class ice_extracter:
     def heat(self, amount):
-
+        return
 
     def move_z(self, amount):
-
+        return
 
 
 class railing:
     def move_x(self, amount):
-
+        return
 
     def move_y(self, amount):
-
-
+        return
