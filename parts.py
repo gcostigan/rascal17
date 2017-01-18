6c2723ddcb7674c2a6026d313b69a0b7ba0a6841
evalynn borrego <evalynn borrego>
2017-01-17 19:31:28 -0500

    notes added a manual override, need to work on break statement

--- parts.py
+++ parts.py
@@ -1,5 +1,9 @@
 import wiringpi
 
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+
+import time
+import atexit
 class trencher:
     def tilt(self, servo, amount):
         self.servo = servo
@@ -16,6 +20,11 @@ class trencher:
 
     def manual(self):
         # Loop to be called when the operator needs manual overdrive
+        while(True):
+            spin.step(100, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+            spin.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
+            # continuously spinning forward or backward, 100 steps is just a guess for now!
+            #need a break statement
         return
 
 
