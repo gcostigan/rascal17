19162c7a321fc1c956ec00e928b0796ed9d7ac30
emilybec <ebeckma1@vols.utk.edu>
2017-01-17 19:51:35 -0500

    added some coding to can and railing

--- parts.py
+++ parts.py
@@ -16,19 +16,25 @@ class trencher:
 
 
 class ice_extracter:
-    def heat(self, amount):
+    def heat(self, on_off): #assuming heating is an on/off operation was can use true/false. If value we can change to amount
+        while(True):
+            #send current to can to heat up
         return
 
     def move_z(self, amount):
+        canStepper.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
         return
 
     def overheat(self,temp):
-        #If temp above threshold shut it off
+        if temp>200 #change this to whatever temp we need
+            #turn off current to can
         return
 
 class railing:
     def move_x(self, amount):
+        railStepperx.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
         return
 
     def move_y(self, amount):
+        railSteppery.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
         return
