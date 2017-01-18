600d4d1317c0751b1944506b663d4abdabab71ff
evalynn borrego <evalynn borrego>
2017-01-17 20:17:58 -0500

    test

--- parts.py
+++ parts.py
@@ -1,24 +1,21 @@
 import wiringpi
 
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
-
-import time
-import atexit
 class trencher:
-    def tilt(self, servo, amount):
+    def tilt(self, amount):
         return
 
-    def spin(self, servo, amount):
+    def spin(self, amount):
+        if (amount > 0):
+            #dir foward
+        else:
+            #dir backward
         return
-
     def origin(self):
         return
 
     def manual(self):
         # Loop to be called when the operator needs manual overdrive
         while(True):
-            spin.step(100, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
-            spin.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
             # continuously spinning forward or backward, 100 steps is just a guess for now!
             #need a break statement
         return
