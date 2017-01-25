27a493e984d4e1258fa710c9c0b3541b19e0c6ce
evalynn borrego <evalynn borrego>
2017-01-24 19:43:01 -0500

    Added debug controller to README.md.

--- parts.py
+++ parts.py
@@ -1,22 +1,37 @@
 import wiringpi
+import sys
+import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+StepPins = [17, 22, 23, 24] #CHANGE NUMBERS WHEN WE KNOW WHAT PINS TO USE!
 
 class trencher:
     def tilt(self, amount):
-        return
+        if (amount > 0):
+            #dir foward
+        else:
+            #dir backward
 
     def spin(self, amount):
         if (amount > 0):
             #dir foward
         else:
             #dir backward
-        return
+
     def origin(self):
         return
 
     def manual(self):
         # Loop to be called when the operator needs manual overdrive
         while(True):
-            # continuously spinning forward or backward, 100 steps is just a guess for now!
+            #continuously spinning forward or backward
             #need a break statement
         return
 
