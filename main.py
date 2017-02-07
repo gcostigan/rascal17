00b4ff77ea8dd77c620d0071a03f751558758735
Glen Costigan <gcostiga@vols.utk.edu>
2017-02-07 18:37:15 -0500

    Xbox controller layouts first configured

--- main.py
+++ main.py
@@ -1,24 +1,79 @@
-from parts import *
 import XboxController
-# http://www.stuffaboutcode.com/2014/10/raspberry-pi-xbox-360-controller-python.html
+import sys
+import time
 
-t = trencher()
-i = ice_extracter()
-r = railing()
-xbox = XboxController.XboxController(
-    controllerCallBack = None,
-    joystickNo = 0,
-    deadzone = 0.1,
-    scale = 1,
-    invertYAxis = False)
+def universalMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.A, canMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.B, trencherMode)
+    xboxCont.start()
 
-def abort():
-    return
+def canMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.B, meltMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.A, positionMode)
+    xboxCont.start()
 
-def manual_control():
-    goto_auto = 0
-    while goto_auto == 0:
-        # Do manual control until toggled out
+def meltMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.B, turnOffPump)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.A, turnOnPump)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.X, toggleHeat)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, zMove)
+    xboxCont.start()
+
+def positionMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, canMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, yMove)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBX, xMove)
+
+def trencherMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.A, digMode)
+    xboxCont.start()
+
+def digMode():
+    xboxCont.stop()
+    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.B, stopBelt)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.A, runBelt)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.RB, rollBeltForwards)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LB, rollBeltBackwards)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.RTHUMBY, tilt)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, zMove)
+    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBX, xMove)
+    xboxCont.start()
 
 if __name__ == "__main__":
-    manual_control()
+    xboxCont = XboxController.XboxController(
+        controllerCallBack=None,
+        joystickNo=0,
+        deadzone=0.1,
+        scale=1,
+        invertYAxis=False)
+
+    try:
+        #start the controller
+        xboxCont.start()
+        print "xbox controller running"
+        universalMode()
+        while True:
+            time.sleep(1)
+
+    #Ctrl C
+    except KeyboardInterrupt:
+        print "User cancelled"
+
+    #error
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
+    finally:
+        #stop the controller
+        xboxCont.stop()
+
