333d67986fef317ac81576991e59c9d7c9134ed1
eborrego <eborrego@vols.utk.edu>
2017-02-08 21:50:04 -0500

    Added debuging text

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def manual_control():
 
 if __name__ == "__main__":
     manual_control()
+
+    """
+    Example usage:
+
+    import xbox
+    joy = xbox.Joystick()         #Initialize joystick
+
+    if joy.A():                   #Test state of the A button (1=pressed, 0=not pressed)
+        print 'A button pressed'
+    x_axis   = joy.leftX()        #X-axis of the left stick (values -1.0 to 1.0)
+    (x,y)    = joy.leftStick()    #Returns tuple containing left X and Y axes (values -1.0 to 1.0)
+    trigger  = joy.rightTrigger() #Right trigger position (values 0 to 1.0)
+
+    joy.close()                   #Cleanup before exit
+    """
+    # Look at these:
+    # https://github.com/FRC4564/Xbox
+    # https://raw.githubusercontent.com/FRC4564/Xbox/master/xbox.py
+    # https://raw.githubusercontent.com/FRC4564/Xbox/master/sample.py
