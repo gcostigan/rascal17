ebec0cadba6538ecb1a345491e2131e5400e2586
eborrego <eborrego@vols.utk.edu>
2017-03-10 15:03:35 -0500

    Changed master branch

--- main.py
+++ main.py
@@ -1,106 +1,44 @@
+from parts import *
 import XboxController
-import sys
-import time
+# http://www.stuffaboutcode.com/2014/10/raspberry-pi-xbox-360-controller-python.html
 
-def universalMode():
-    print "Entering Universal Mode... "
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, canMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.B, trencherMode)
-    xboxCont.start()
-    print "Ready"
+t = trencher()
+i = ice_extracter()
+r = railing()
+xbox = XboxController.XboxController(
+    controllerCallBack = None,
+    joystickNo = 0,
+    deadzone = 0.1,
+    scale = 1,
+    invertYAxis = False)
 
-def canMode():
-    print "Entering Can Mode..."
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.B, meltMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, positionMode)
-    xboxCont.start()
-    print "Ready"
+def abort():
+    return
 
-def meltMode():
-    print "Entering Melt Mode..."
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.B, turnOffPump)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, turnOnPump)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.X, toggleHeat)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, zMove)
-    xboxCont.start()
-    print "Ready"
-
-def positionMode():
-    print "Entering Position Mode..."
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, canMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, yMove)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBX, xMove)
-    xboxCont.start()
-    print "Ready"
-
-def trencherMode():
-    print "Entering Trencher Mode..."
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, digMode)
-    xboxCont.start()
-    print "Ready"
-
-def digMode():
-    print "Entering Dig Mode..."
-    print "Stopping controller..."
-    xboxCont.stop()
-    print "Changing layout..."
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, universalMode)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.B, stopBelt)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, runBelt)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.RB, rollBeltForwards)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LB, rollBeltBackwards)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.RTHUMBY, tilt)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, zMove)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBX, xMove)
-    xboxCont.start()
-    print "Ready"
+def manual_control():
+    goto_auto = 0
+    while goto_auto == 0:
+        # Do manual control until toggled out
 
 if __name__ == "__main__":
-    xboxCont = XboxController.XboxController(
-        controllerCallBack=None,
-        joystickNo=0,
-        deadzone=0.1,
-        scale=1,
-        invertYAxis=False)
+    manual_control()
 
-    try:
-        #start the controller
-        xboxCont.start()
-        print "Xbox controller running"
-        universalMode()
-        print "Main loop started"
-        while True:
-            time.sleep(1)
+    """
+    Example usage:
 
-    #Ctrl C
-    except KeyboardInterrupt:
-        print "User cancelled"
+    import xbox
+    joy = xbox.Joystick()         #Initialize joystick
 
-    #error
-    except:
-        print "Unexpected error:", sys.exc_info()[0]
-        raise
+    if joy.A():                   #Test state of the A button (1=pressed, 0=not pressed)
+        print 'A button pressed'
+    x_axis   = joy.leftX()        #X-axis of the left stick (values -1.0 to 1.0)
+    (x,y)    = joy.leftStick()    #Returns tuple containing left X and Y axes (values -1.0 to 1.0)
+    trigger  = joy.rightTrigger() #Right trigger position (values 0 to 1.0)
 
-    finally:
-        #stop the controller
-        xboxCont.stop()
+    joy.close()                   #Cleanup before exit
+    """
+    # Look at these:
+    # https://github.com/FRC4564/Xbox
+    # https://raw.githubusercontent.com/FRC4564/Xbox/master/xbox.py
+    # https://raw.githubusercontent.com/FRC4564/Xbox/master/sample.py
 
-#edit
--- parts.py
+++ parts.py
@@ -10,10 +10,115 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17, 22, 23, 24] #CHANGE NUMBERS WHEN WE KNOW WHAT PINS TO USE!
 
-class trencher:
-    def tilt(self, amount):
+
+class Motor(object):
+    #intialize motors
+    def _init_(self,pins):
+        self.P1 = pins[0]
+        self.P2 = pins[1]
+        self.P3 = pins[2]
+        self.p4 = pins[3]
+        self.P5 = pins[4]
+        self.P6 = pins[5]
+        self.deg_per_step = 5.625 / 64
+        self.steps_per_rev = int(360 / self.deg_per_step)
+        self.step_angle = 0 #assume the way its pointing is zero
+        for p in pins:
+            GPIO.setup(p,GPIO.out)
+            GPIO.output(p,0)
+
+    def _set_rpm(self, rpm):
+        '''Set the turn speed in RPM'''
+        self._rpm = rpm
+        self._T = (60.0 / rpm) / self.steps_per_rev
+        ## T is the amount of time to stop between signals
+
+    # This means you can set "rpm" as if it is an attribute and behind the scenes
+    # its sets _T sttribute
+    rpm = property(lambda self: self._rpm, _set_rpm
+
+    def move_to(self, angle):
+        '''Take the shortest route to a particular angle (degrees).'''
+        #Make sure there is a 1:1 mapping between angle and stepper angle
+
+        target_set_angle = 8 * (int(angle / self.deg_per_step) / 8)
+        steps = target_set_angle - self.step_angle
+        steps = (steps % self.steps_per_rev)
+        if steps > self.steps_per_rev / 2:
+            steps -= self.steps_per_rev
+            print "moving" + `steps` + "steps"
+            self._move_acw(-steps / 8)
+        else:
+            print "moving" + `steps` + "steps"
+            self._move_cw(steps / 8)
+        self.step_angle = target_step_angle
+
+    def _move_acw(selfself, big_steps):
+        GPIO.output(self.P1, 0)
+        GPIO.output(self.P2, 0)
+        GPIO.output(self.P3, 0)
+        GPIO.output(self.P4, 0)
+        GPIO.output(self.P5, 0)
+        GPIO.output(self.P6, 0)
+        for i range(big_steps):
+            GPIO.output(self.P1, 0)
+            sleep(self._T)
+            GPIO.output(self.P2, 0)
+            sleep(self._T)
+            GPIO.output(self.P3, 0)
+            sleep(self._T)
+            GPIO.output(self.P4, 0)
+            sleep(self._T)
+            GPIO.output(self.P5, 0)
+            sleep(self._T)
+            GPIO.output(self.P6, 0)
+            sleep(self._T)
+
+    def _move_cw(selfself, big_steps):
+        GPIO.output(self.P1, 0)
+        GPIO.output(self.P2, 0)
+        GPIO.output(self.P3, 0)
+        GPIO.output(self.P4, 0)
+        GPIO.output(self.P5, 0)
+        GPIO.output(self.P6, 0)
+        for i range(big_steps):
+            GPIO.output(self.P1, 0)
+            sleep(self._T)
+            GPIO.output(self.P2, 0)
+            sleep(self._T)
+            GPIO.output(self.P3, 0)
+            sleep(self._T)
+            GPIO.output(self.P4, 0)
+            sleep(self._T)
+            GPIO.output(self.P5, 0)
+            sleep(self._T)
+            GPIO.output(self.P6, 0)
+            sleep(self._T)
+'''
+if _name_ == "_main_":
+    GPIO.setmode(GPIO.BOARD)
+    m = Motor([]) ## NEED to input location of motors
+    m.rpm = 5
+    print "Pause in seconds: " + `m._T`
+    m.move_to(90)
+    sleep(1)
+    m.move_to(0)
+    sleep(1)
+    m.move_to(-90)
+    sleep(1)
+    m.move_to(-180)
+    sleep(1)
+    m.move_to(0)
+    GPIO.cleanup()
+
+    .END
+
+    ##This section confuses me -.-
+    '''
+
+'''
+ def tilt(self, amount):
         if (amount > 0):
             #dir foward
         else:
@@ -25,6 +130,12 @@ class trencher:
             #dir foward
         else:
             #dir backward
+        return
+
+    def move_x (self, amount):
+        return
+
+    def move_y (self, amount):
         return
 
     def origin(self):
@@ -44,26 +155,20 @@ class ice_extracter:
             #send current to can to heat up
         return
 
-    def move_z(self, amount):
-        canStepper.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
-        return
-
-    def overheat(self,temp):
-        if temp>200 #change this to whatever temp we need
-            #turn off current to can
-        return
-
-class railing:
     def move_x(self, amount):
-        railStepper_x.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
         return
 
     def move_y(self, amount):
-        railStepper_y.step(amount, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
         return
 
-class pump:
-    def run_pump(self, pump_on_off):
-        while(True)
-            #run pump
+    def move_z(self, amount):
         return
+
+    def overheat(self,temp):
+        if temp > 200 #change this to whatever temp we need
+            #turn off current to can
+        return
+
+    def pump(self,pump_on_off):
+        return
+'''
