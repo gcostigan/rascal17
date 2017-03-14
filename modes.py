036b0b830b684739e45b11e1d457b9f8da1c165e
Glen Costigan <gcostiga@vols.utk.edu>
2017-03-14 13:55:44 -0400

    cleaned up modes

--- modes.py
+++ modes.py
@@ -1,16 +1,15 @@
 import XboxController
 
-global Heat, Pump, Belt
-Heat, Pump, Belt = False, False, False
+class Modes:
 
-class modes:
-    
     def __init__(self):
         self.controller = XboxController.XboxController(
             controllerCallBack=None,
             deadzone=30,
             scale=100,
             invertYAxis=True)
+        global Heat, Pump, Belt
+        Heat, Pump, Belt = False, False, False
 
     @staticmethod
     def empty(value):
@@ -28,7 +27,7 @@ class modes:
         if value == 1:
             print "Entering Universal Mode... "
             self.controller.setupControlCallback(self.controller.XboxControls.A, self.can)
-            self.controller.setupControlCallback(self.controller.XboxControls.B, self.trencherMode)
+            self.controller.setupControlCallback(self.controller.XboxControls.B, self.trencher_mode)
             self.controller.setupControlCallback(self.controller.XboxControls.X, self.empty)
             self.controller.setupControlCallback(self.controller.XboxControls.Y, self.empty)
             self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBY, self.empty)
@@ -97,32 +96,31 @@ class modes:
             self.controller.setupControlCallback(self.controller.XboxControls.RTHUMBY, self.empty)
             print "Ready"
         return
-    
-    
+
     def position(self, value):
-        def yMove(value):
-            if value > 1:
+        def y_move(val):
+            if val > 1:
                 print "Move Left"
-            if value < 1:
+            if val < 1:
                 print "Move Right"
-            if value == 0:
+            if val == 0:
                 print "Stop y movement."
             return
     
-        def xMove(value):
-            if value > 1:
+        def x_move(val):
+            if val > 1:
                 print "Move forwards"
-            if value < 1:
+            if val < 1:
                 print "Move backwards"
-            if value == 0:
+            if val == 0:
                 print "Stop x movement."
             return
     
         if value == 1:
             print "Entering Position Mode..."
             self.controller.setupControlCallback(self.controller.XboxControls.Y, self.can)
-            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBY, yMove)
-            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBX, xMove)
+            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBY, y_move)
+            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBX, x_move)
             self.controller.setupControlCallback(self.controller.XboxControls.X, self.empty)
             self.controller.setupControlCallback(self.controller.XboxControls.A, self.empty)
             self.controller.setupControlCallback(self.controller.XboxControls.B, self.empty)
@@ -131,9 +129,8 @@ class modes:
             self.controller.setupControlCallback(self.controller.XboxControls.RTHUMBY, self.empty)
             print "Ready"
         return
-    
-    
-    def trencherMode(self, value):
+
+    def trencher_mode(self, value):
         if value == 1:
             print "Entering Trencher Mode..."
             self.controller.setupControlCallback(self.controller.XboxControls.Y, self.universal)
@@ -147,57 +144,56 @@ class modes:
             self.controller.setupControlCallback(self.controller.XboxControls.RTHUMBY, self.empty)
             print "Ready"
         return
-    
-    
+
     def dig(self, value):
-        def toggleBelt(value):
-            if value == 1:
+        def toggle_belt(val):
+            if val == 1:
                 global Belt
                 Belt = not Belt
-                if Belt == True:
+                if Belt:
                     print "Belt is on."
                 else:
                     print "Belt is off."
             return
     
-        def rollBeltForwards(value):
-            if value == 1:
+        def roll_belt_forwards(val):
+            if val == 1:
                 print "Roll Belt Forwards"
-            if value == 0:
+            if val == 0:
                 print "Stop Belt movement."
             return
     
-        def rollBeltBackwards(value):
-            if value == 1:
+        def roll_belt_backwards(val):
+            if val == 1:
                 print "Roll Belt Backwards"
-            if value == 0:
+            if val == 0:
                 print "Stop Belt Movement"
             return
     
-        def tilt(value):
-            if value > 1:
+        def tilt(val):
+            if val > 1:
                 print "Tilt trencher forwards"
-            if value < 1:
+            if val < 1:
                 print "Tilt trencher backwards"
-            if value == 0:
+            if val == 0:
                 print "Stopping Trencher tilt movement"
             return
     
-        def zMove(value):
-            if value > 1:
+        def z_move(val):
+            if val > 1:
                 print "Move Trencher Up"
-            if value < 1:
+            if val < 1:
                 print "Move Trencher Down"
-            if value == 0:
+            if val == 0:
                 print "Stopping Trencher z movement"
             return
     
-        def xMove(value):
-            if value > 1:
+        def x_move(val):
+            if val > 1:
                 print "Move Trencher forwards"
-            if value < 1:
+            if val < 1:
                 print "Move Trencher backwards"
-            if value == 0:
+            if val == 0:
                 print "Stopping Trencher x movement"
             return
     
@@ -205,12 +201,12 @@ class modes:
             print "Entering Dig Mode..."
             self.controller.setupControlCallback(self.controller.XboxControls.Y, self.universal)
             self.controller.setupControlCallback(self.controller.XboxControls.B, self.empty)
-            self.controller.setupControlCallback(self.controller.XboxControls.A, toggleBelt)
-            self.controller.setupControlCallback(self.controller.XboxControls.RB, rollBeltForwards)
-            self.controller.setupControlCallback(self.controller.XboxControls.LB, rollBeltBackwards)
+            self.controller.setupControlCallback(self.controller.XboxControls.A, toggle_belt)
+            self.controller.setupControlCallback(self.controller.XboxControls.RB, roll_belt_forwards)
+            self.controller.setupControlCallback(self.controller.XboxControls.LB, roll_belt_backwards)
             self.controller.setupControlCallback(self.controller.XboxControls.RTHUMBY, tilt)
-            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBY, zMove)
-            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBX, xMove)
+            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBY, z_move)
+            self.controller.setupControlCallback(self.controller.XboxControls.LTHUMBX, x_move)
             self.controller.setupControlCallback(self.controller.XboxControls.X, self.empty)
             print "Ready"
         return
